58113bb7967c3b108b9ae1c28cb90b7a4441c13c
Wassim CHERIFI <wassim.cherifi@rentplus.eu>
2024-03-01 15:08:12 +0100

    commit dev1

--- Back/FileBack1.cs
+++ Back/FileBack1.cs
@@ -5,6 +5,7 @@ public class RandomClass
     Console.log("Hello world");
     Console.log("This is the first change!");
     Console.log("ignoring file 4");
+    Console.log("this is the development of developer 1");
     //lines 2
     //lines 3
 }
